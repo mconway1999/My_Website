3c4fd06a1c2e98a1db14c851faa49b172fdc0ad8
Madeline Conway <conway.maddy@yahoo.com>
2024-08-14 22:01:58 -0400

    completed

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This is my first website!";
 document.querySelector("body").appendChild(h2);
